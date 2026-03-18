5092e194f366ab53d164cf294d9654b814056a0f
ThanhLeoChohub <meobellrbl@gmail.com>
2026-03-18 19:34:17 +0700

    Add LEOCHO.lua with script metadata and loading function

---
+++ LEOCHO.lua
@@ -0,0 +1,6 @@
+getgenv().ScriptTitle = "DoMiXi hub"
+getgenv().ScriptSubTitle = "by Shin Roblox"
+getgenv().ScriptImage = "https://i.ibb.co/NdP1DySP/Screenshot-2026-03-18-19-14-54-06.png"
+getgenv().ScriptAuthorName = "DoMiXi hub"
+getgenv().ScriptAuthorSubTitle = "Author Info"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hoannhatz/Script/refs/heads/main/EZScript.lua"))()
