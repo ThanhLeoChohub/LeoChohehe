2df0b7339fa04d2e2b90065a74fb71cd24e419f7
ThanhLeoChohub <meobellrbl@gmail.com>
2026-03-18 20:29:54 +0700

    Update LEOCHO.lua

--- LEOCHO.lua
+++ LEOCHO.lua
@@ -1,6 +1,7 @@
+-- this script created at https://lambloxfruitscript.vercel.app/
 getgenv().ScriptTitle = "DoMiXi hub"
-getgenv().ScriptSubTitle = "by Shin Roblox"
+getgenv().ScriptSubTitle = "V3.6.7"
 getgenv().ScriptImage = "https://i.ibb.co/NdP1DySP/Screenshot-2026-03-18-19-14-54-06.png"
-getgenv().ScriptAuthorName = "DoMiXi hub"
-getgenv().ScriptAuthorSubTitle = "Author Info"
+getgenv().ScriptAuthorName = "SHIN ROBLOX"
+getgenv().ScriptAuthorSubTitle = "Tik Tok: @honglocheo"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/hoannhatz/Script/refs/heads/main/EZScript.lua"))()
